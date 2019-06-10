daea0b64d7eba38743b711b8aa75ac353d2d552b
erick2014 <erickarmandogarcia@hotmail.com>
2019-06-10 08:25:12 -0500

    format code and pass props object to HomePage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Router, Route,Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { PrivateRoute } from './PrivateRoute.js';
 import { history } from './helpers';
-import { alertActions } from './actions';
+import { alertActions, userActions } from './actions';
 
 // @components
-import { HomePage } from './components/HomePage';
+import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 
@@ -14,27 +14,26 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-       history.listen((location, action) => {
-           
-       });
+        history.listen((location, action) => {
+
+        });
     }
-    
-    renderAlertMessage(){
+
+    renderAlertMessage() {
         const {
-            alert:{ type, message },
+            alert: { type, message },
             clearAlert
         } = this.props;
-        
-        if( !type ){
+
+        if (!type) {
             return null;
         }
-        
-        
-        return(
-            <div className={'alert '+ type }>
+
+        return (
+            <div className={'alert ' + type}>
                 {message}.
-                <button 
-                    type="button" 
+                <button
+                    type="button"
                     className="close"
                     data-dismiss="alert"
                     onClick={clearAlert}
@@ -44,21 +43,17 @@ class App extends React.Component {
     }
 
     render() {
-
-        const {authentication} = this.props;
         return (
-
-              <div className="container">
+            <div className="container">
                 {this.renderAlertMessage()}
-                 <div className="col-sm-8 col-sm-offset-2">
-                   <Switch>
-                        <Route exact path="/" render={()=> <HomePage {...authentication} /> } />
+                <div className="col-sm-8 col-sm-offset-2">
+                    <Switch>
+                        <Route exact path="/" render={() => <HomePage {...this.props} />} />
                         <Route exact path="/signup" component={RegisterPage} />
                         <Route exact path="/login" component={LoginPage} />
                     </Switch>
-                 </div>
-              </div>
-
+                </div>
+            </div>
         );
     }
 }
@@ -69,8 +64,9 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = {
-    clearAlert: alertActions.clear
+    clearAlert: alertActions.clear,
+    logout: userActions.logout
 }
 
 
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
