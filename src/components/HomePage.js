8a8dc83faf260f7d0a04e83c9b500c9c942cbe15
erick2014 <erickarmandogarcia@hotmail.com>
2019-06-10 08:24:12 -0500

    refactor HomePage component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export class HomePage extends Component {
+const HomePage = ({ authentication, history, logout }) => {
 
-    render() {
-        const {
-            authentication,
-            history
-        } = this.props;
-
-        if (authentication && !authentication.loggedIn) {
-            history.push("/login")
-        }
-
-        return (
-            <div className="col-md-6 col-md-offset-3">
-                <h2 align="center">Welcome! You have successfully logged in.</h2>
-                <p align="center">
-                // Add a redirection for logout
-                </p>
-            </div>
-        );
+    if (authentication && !authentication.loggedIn) {
+        history.push("/login")
     }
+
+    return (
+        <div className="col-md-6 col-md-offset-3">
+            <h2 align="center">Welcome! You have successfully logged in.</h2>
+            <p align="center">
+                <Link to="/login" className="btn btn-link" onClick={logout}>Logout</Link>
+            </p>
+        </div>
+    );
 }
+
+export default HomePage;
